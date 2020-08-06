be3376d5bd3e5c361538a5af45f8434f612ff2af
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-06 12:16:40 +0300

    add like button

--- index.php
+++ index.php
@@ -28,22 +28,34 @@
       button.favorite-button:hover {
         background: #000099;
       }
-
+        .favorite-heart {
+            color: red;
+            font-size: 2em;
+            float: right;
+            display: none;
+        }
+        .favorite .favorite-heart {
+            display: block;
+        }
     </style>
   </head>
   <body>
+   <?php echo join(', ', $_SESSION['favorites']); ?>
     <div id="blog-posts">
       <div id="blog-post-101" class="blog-post">
+       <div class="favorite-heart">&hearts;</div>
         <h3>Blog Post 101</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
         <button class="favorite-button">Favorite</button>
       </div>
       <div id="blog-post-102" class="blog-post">
+       <div class="favorite-heart">&hearts;</div>
         <h3>Blog Post 102</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
         <button class="favorite-button">Favorite</button>
       </div>
       <div id="blog-post-103" class="blog-post">
+       <div class="favorite-heart">&hearts;</div>
         <h3>Blog Post 103</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
         <button class="favorite-button">Favorite</button>
@@ -61,6 +73,9 @@
           if(xhr.readyState == 4 && xhr.status == 200) {
             var result = xhr.responseText;
             console.log('Result: ' + result);
+              if(result == 'true') {
+                  parent.classList.add("favorite");
+              }
           }
         };
         xhr.send("id=" + parent.id);
